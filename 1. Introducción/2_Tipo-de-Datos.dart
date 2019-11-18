6d1ba59f618ea420db6fadba6b18a41aebfc4a9e
daniel <menacanaviridaniel@gmail.com>
2019-11-17 23:42:45 -0400

    int y string

---
+++ 1. Introducción/2_Tipo-de-Datos.dart
@@ -0,0 +1,24 @@
+void main() {
+  //Números
+  int numero1 = 10;
+  double pi = 3.1416;
+  print('$numero1 - $pi');
+
+  //Dart segun el valor infiere que es una variable que almacenara datos de tipo double
+  var numero2 = 1.0;
+  print(numero2);
+
+  //String - Cadena de Caracteres
+  String cadena1 = "Esta es una cadena de caracteres";
+  print(cadena1);
+  String cadena2 = '"Esta es una cadena de caracteres entre comillas dobles"';
+  print(cadena2);
+  String cadena3 = "'Esta es una cadena de caracteres entre comillas simples'";
+  print(cadena3);
+  String cadena4 = 'Esta es otra \'cadena\' de caracteres';
+  print(cadena4);
+
+  
+
+
+}

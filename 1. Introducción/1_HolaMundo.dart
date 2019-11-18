93cd9b8c5436b7688ef7f67ec3ad6b9f333e0ae8
daniel <menacanaviridaniel@gmail.com>
2019-11-17 21:54:21 -0400

    1er commit Hola mundo

---
+++ 1. Introducción/1_HolaMundo.dart
@@ -0,0 +1,16 @@
+void main() {
+
+  // Asi se muestra un comentario de una linea 
+
+  /*
+    Asi se escribe un comentario de ´
+    multiples lineas
+  */
+
+  //Mostrar un Mensaje
+  print ("Hola Mundo con Dart");
+
+  //Variables y mostrar mensaje con variables
+  String nombre = 'Daniel';
+  print('Hola $nombre');
+}
